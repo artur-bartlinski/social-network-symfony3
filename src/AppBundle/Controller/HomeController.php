e00982b8f858225ac1b04dbaab1c694c44f3393a
Artur <artur@localhost>
2017-11-02 19:00:28 +0000

    Added Bootstrap and jQuery libraries, created base view, added navigation bar and flash messages

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('home/index.html.twig', array(
+            // ...
+        ));
+    }
+}
